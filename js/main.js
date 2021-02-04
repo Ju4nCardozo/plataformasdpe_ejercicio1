76c6961dacb42aafbdc5cecde4464ae88812a425
Ju4nCardozo <juan.cardozog@upb.edu.co>
2021-02-04 04:17:42 -0500

    Estructurando la página de la galeria

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
 ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.cartas' , {delay: 500});
+ScrollReveal().reveal('.showcase_gallery');
+ScrollReveal().reveal('.cartas_gallery' , {delay: 500});
